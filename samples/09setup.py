3eecbdc3ccb5c899791245d75a01738a609b73a0
Petr Viktorin <encukou@gmail.com>
2010-11-23 11:31:39 +0200

    Add the 9th lecture

---
+++ samples/09setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+setup(
+    name = "HelloWorld",
+    version = "0.1",
+    packages = find_packages(),
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires = ['docutils>=0.3'],
+
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+        # And include any *.msg files found in the 'hello' package, too:
+        'hello': ['*.msg'],
+    }
+
+    # metadata for upload to PyPI
+    author = "Me",
+    author_email = "me@example.com",
+    description = "This is an Example Package",
+    license = "PSF",
+    keywords = "hello world example examples",
+    url = "http://example.com/HelloWorld/",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
