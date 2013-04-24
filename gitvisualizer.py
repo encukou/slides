7a5013cfd7b8ce13de76aef0a5356831b74dd3d0
Petr Viktorin <encukou@gmail.com>
2013-04-24 15:31:57 +0200

    Start working on the visualization

--- gitvisualizer.py
+++ gitvisualizer.py
@@ -2,11 +2,16 @@ from __future__ import print_function, unicode_literals, division
 
 import subprocess
 import sys
+from math import pi, sin, cos
 
+from pyglet import gl
+from gillcup_graphics import Layer, GraphicsObject, Window, RealtimeClock, run
 
-def run(*command):
+tau = 2 * pi
+
+def run_git(*command):
     print(command)
-    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    process = subprocess.Popen(('git',) + command, stdout=subprocess.PIPE)
     return process.stdout
 
 
@@ -16,14 +21,70 @@ def visclass(cls):
     return cls
 
 
+def circle(x, y, radius):
+    iterations = int(tau * radius) * 2
+    s = sin(tau / iterations)
+    c = cos(tau / iterations)
+    gl.glBegin(gl.GL_TRIANGLE_FAN)
+    gl.glVertex2f(x, y)
+    dx, dy = radius, 0
+    for i in range(iterations + 1):
+        gl.glVertex2f(x + dx, y + dy)
+        dx, dy = (dx * c - dy * s), (dy * c + dx * s)
+    gl.glEnd()
+
+
+class Object(GraphicsObject):
+    def __init__(self, graph, name):
+        GraphicsObject.__init__(self)
+        self.graph = graph
+        self.name = name
+
+        self.radius = 10
+        self.pointers_in = set()
+        self.drag_starts = {}
+
+    def draw(self, **kwargs):
+        if self.pointers_in:
+            gl.glColor4f(1, 0, 0, 1)
+        else:
+            gl.glColor4f(1, 1, 1, 1)
+        circle(self.x, self.y, self.radius + 2)
+        gl.glColor4f(0, 0, 0, 1)
+        circle(self.x, self.y, self.radius)
+
+    def hit_test(self, x, y, z):
+        return x ** 2 + y ** 2 < self.radius ** 2
+
+    def on_pointer_motion(self, pointer, x, y, z, **kwargs):
+        self.pointers_in.add(pointer)
+        return True
+
+    def on_pointer_leave(self, pointer, x, y, z, **kwargs):
+        self.pointers_in.remove(pointer)
+        return True
+
+    def on_pointer_press(self, pointer, x, y, z, button, **kwargs):
+        self.drag_starts[pointer, button] = x, y
+        return True
+
+    def on_pointer_drag(self, pointer, x, y, z, button, **kwargs):
+        start_x, start_y = self.drag_starts[pointer, button]
+        self.position = self.x + x - start_x, self.y + y - start_y
+        return True
+
+    def on_pointer_release(self, pointer, x, y, z, button, **kwargs):
+        del self.drag_starts[pointer, button]
+        return True
+
+
 @visclass
-class Commit(object):
+class Commit(Object):
     type = 'commit'
     def __init__(self, graph, sha):
-        self.graph = graph
-        self.name = sha
+        Object.__init__(self, graph, sha)
         self.parents = []
-        stdout = run('git', 'cat-file', sha, '-p')
+        stdout = run_git('cat-file', sha, '-p')
         for line in stdout:
             line = line.strip()
             name, space, value = line.partition(' ')
@@ -49,14 +110,13 @@ class Commit(object):
         return self.message.partition('\n')[0]
 
 @visclass
-class Tree(object):
+class Tree(Object):
     type = 'tree'
     summary = None
     def __init__(self, graph, sha):
-        self.graph = graph
-        self.name = sha
+        Object.__init__(self, graph, sha)
         self.children = {}
-        for line in run('git', 'cat-file', sha, '-p'):
+        for line in run_git('cat-file', sha, '-p'):
             line = line.strip()
             info, tab, filename = line.partition('\t')
             obj = self.add_child(info, filename)
@@ -70,20 +130,18 @@ class Tree(object):
 
 
 @visclass
-class Blob(object):
+class Blob(Object):
     type = 'blob'
     summary = None
     def __init__(self, graph, sha):
-        self.graph = graph
-        self.name = sha
-        self.contents = run('git', 'cat-file', sha, '-p').read()
+        Object.__init__(self, graph, sha)
+        self.contents = run_git('cat-file', sha, '-p').read()
 
 
-class Ref(object):
+class Ref(Object):
     type = 'ref'
     def __init__(self, graph, name, target):
-        self.graph = graph
-        self.name = name
+        Object.__init__(self, graph, name)
         self.target = target
         graph.add_edge(self, target, RefTarget)
 
@@ -126,8 +184,9 @@ class TreeEntry(Edge):
         return './' + self.filename
 
 
-class Graph(object):
-    def __init__(self, repo_path):
+class Graph(Layer):
+    def __init__(self, repo_path, clock):
+        Layer.__init__(self)
         self.repo_path = repo_path
         self.objects = {}
         self.edges = {}
@@ -136,11 +195,11 @@ class Graph(object):
     def update(self):
         self.lingering = self.objects
         self.objects = {}
-        for line in run('git', 'show-ref'):
+        for line in run_git('show-ref'):
             sha, name = line.strip().split()
             obj = self.add_object(sha)
             self.add_ref(name, obj)
-        for line in run('git', 'symbolic-ref', 'HEAD'):
+        for line in run_git('symbolic-ref', 'HEAD'):
             name = line.strip()
             self.add_ref('HEAD', self.add_ref(name))
 
@@ -161,14 +220,14 @@ class Graph(object):
     @_add
     def add_ref(self, name, target=None):
         if target is None:
-            for line in run('git', 'rev-parse', name):
+            for line in run_git('rev-parse', name):
                 target = self.add_object(line.strip())
         assert target is not None
         return Ref(self, name, target)
 
     @_add
     def add_object(self, sha):
-        obj_type = run('git', 'cat-file', sha, '-t').read().strip()
+        obj_type = run_git('cat-file', sha, '-t').read().strip()
         try:
             visclass = visclass_by_type[obj_type]
         except KeyError:
@@ -196,10 +255,29 @@ class Graph(object):
                 if b is obj:
                     print(' <-', edge.summary, '--', a.name)
 
+    @property
+    def children(self):
+        return self.objects.values()
+
+    @children.setter
+    def children(self, v):
+        pass
+
+
+class GVWindow(Window):
+    def on_resize(self, width, height):
+        super(Window, self).on_resize(width, height)
+        layer = self.layer
+        layer.position = width / 2, height / 2
+
 
 def main(repo_path):
-    g = Graph(repo_path)
+    clock = RealtimeClock()
+    g = Graph(repo_path, clock)
     g.dump()
+
+    GVWindow(g, width=400, height=400, resizable=True)
+    run()
 
 
 if __name__ == '__main__':
