cc3661a45b40ba20976ad6dad2075ea518a4be0e
Petr Viktorin <pviktori@redhat.com>
2014-01-30 23:27:08 +0100

    Add license blob to Europython talk

--- 2012-11-29-pyvo-cli/slides.py
+++ 2012-11-29-pyvo-cli/slides.py
@@ -1,6 +1,26 @@
 #! /bin/env python2
 # Encoding: UTF-8
 
+# Copyright (C) 2011-2013 by Petr Viktorin <encukou@gmail.com>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from __future__ import unicode_literals
 
 import os
@@ -17,7 +37,7 @@ def addattrs(string):
         if '≈' in emph:
             prop, sep, emph = emph.partition('≈')
         else:
-            prop, emph = 'emph', emph
+            prop = 'emph'
         if emph:
             result.append((prop, emph))
     if not result:
