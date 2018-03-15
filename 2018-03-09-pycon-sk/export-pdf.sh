de32b0a09ec65bf5f230db551685997799a67ea5
Petr Viktorin <encukou@gmail.com>
2018-03-15 11:22:37 +0100

    PyCon SK talk: include background color in PDF; add to main README

--- 2018-03-09-pycon-sk/export-pdf.sh
+++ 2018-03-09-pycon-sk/export-pdf.sh
@@ -3,6 +3,8 @@
 # This is a quick-and dirty script; may not work for you
 
 rm -fv *.pdf
-python2 ~/dev/svg-objects-export/svg-objects-export.py --xpath "//svg:g[@inkscape:groupmode='layer']" --extra '--export-area-page --export-id-only' -t pdf slides/slides.svg
+python2 ~/dev/svg-objects-export/svg-objects-export.py --xpath "//svg:g[@inkscape:groupmode='layer']" \
+	--extra '--export-area-page --export-id-only --export-background=#FFFFFFFF' \
+	-t pdf slides/slides.svg
 mutool merge -o slides.pdf slides_l*.pdf
 
